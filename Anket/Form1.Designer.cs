69b4ddad4a9d4ebb64846c27e85b6be43d5d6271
NXbi_av4i@itstep.edu.az <NXbi_av4i@itstep.edu.az>
2022-05-25 23:49:27 +0400

    Add project files.

---
+++ Anket/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿
+namespace Anket
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_load = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textbx_FName_ = new System.Windows.Forms.TextBox();
+            this.textbx_surname = new System.Windows.Forms.TextBox();
+            this.textbx_Patronymic = new System.Windows.Forms.TextBox();
+            this.textbx_Country = new System.Windows.Forms.TextBox();
+            this.textbx_city = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.rbtn_Male = new System.Windows.Forms.RadioButton();
+            this.rbtn_Female = new System.Windows.Forms.RadioButton();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(136, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btn_load
+            // 
+            this.btn_load.Location = new System.Drawing.Point(307, 11);
+            this.btn_load.Name = "btn_load";
+            this.btn_load.Size = new System.Drawing.Size(75, 23);
+            this.btn_load.TabIndex = 1;
+            this.btn_load.Text = "Load";
+            this.btn_load.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.maskedTextBox1);
+            this.groupBox1.Controls.Add(this.rbtn_Female);
+            this.groupBox1.Controls.Add(this.rbtn_Male);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textbx_city);
+            this.groupBox1.Controls.Add(this.textbx_Country);
+            this.groupBox1.Controls.Add(this.textbx_Patronymic);
+            this.groupBox1.Controls.Add(this.textbx_surname);
+            this.groupBox1.Controls.Add(this.textbx_FName_);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(42, 59);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(377, 306);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Anket";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // textbx_FName_
+            // 
+            this.textbx_FName_.Location = new System.Drawing.Point(124, 23);
+            this.textbx_FName_.Name = "textbx_FName_";
+            this.textbx_FName_.Size = new System.Drawing.Size(237, 23);
+            this.textbx_FName_.TabIndex = 1;
+            // 
+            // textbx_surname
+            // 
+            this.textbx_surname.Location = new System.Drawing.Point(124, 52);
+            this.textbx_surname.Name = "textbx_surname";
+            this.textbx_surname.Size = new System.Drawing.Size(237, 23);
+            this.textbx_surname.TabIndex = 2;
+            // 
+            // textbx_Patronymic
+            // 
+            this.textbx_Patronymic.Location = new System.Drawing.Point(124, 81);
+            this.textbx_Patronymic.Name = "textbx_Patronymic";
+            this.textbx_Patronymic.Size = new System.Drawing.Size(237, 23);
+            this.textbx_Patronymic.TabIndex = 3;
+            // 
+            // textbx_Country
+            // 
+            this.textbx_Country.Location = new System.Drawing.Point(124, 133);
+            this.textbx_Country.Name = "textbx_Country";
+            this.textbx_Country.Size = new System.Drawing.Size(237, 23);
+            this.textbx_Country.TabIndex = 4;
+            // 
+            // textbx_city
+            // 
+            this.textbx_city.Location = new System.Drawing.Point(124, 162);
+            this.textbx_city.Name = "textbx_city";
+            this.textbx_city.Size = new System.Drawing.Size(237, 23);
+            this.textbx_city.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Surname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Patronymic";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Country";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 170);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "City";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 199);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 15);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Phone_Number";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(27, 232);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 15);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Date of birth";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(27, 265);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 15);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Gender";
+            // 
+            // rbtn_Male
+            // 
+            this.rbtn_Male.AutoSize = true;
+            this.rbtn_Male.Location = new System.Drawing.Point(112, 265);
+            this.rbtn_Male.Name = "rbtn_Male";
+            this.rbtn_Male.Size = new System.Drawing.Size(51, 19);
+            this.rbtn_Male.TabIndex = 14;
+            this.rbtn_Male.TabStop = true;
+            this.rbtn_Male.Text = "Male";
+            this.rbtn_Male.UseVisualStyleBackColor = true;
+            // 
+            // rbtn_Female
+            // 
+            this.rbtn_Female.AutoSize = true;
+            this.rbtn_Female.Location = new System.Drawing.Point(236, 263);
+            this.rbtn_Female.Name = "rbtn_Female";
+            this.rbtn_Female.Size = new System.Drawing.Size(63, 19);
+            this.rbtn_Female.TabIndex = 15;
+            this.rbtn_Female.TabStop = true;
+            this.rbtn_Female.Text = "Female";
+            this.rbtn_Female.UseVisualStyleBackColor = true;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(124, 191);
+            this.maskedTextBox1.Mask = "(999) 000-0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(237, 23);
+            this.maskedTextBox1.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn_load);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Anket";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_load;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.RadioButton rbtn_Female;
+        private System.Windows.Forms.RadioButton rbtn_Male;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textbx_city;
+        private System.Windows.Forms.TextBox textbx_Country;
+        private System.Windows.Forms.TextBox textbx_Patronymic;
+        private System.Windows.Forms.TextBox textbx_surname;
+        private System.Windows.Forms.TextBox textbx_FName_;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
